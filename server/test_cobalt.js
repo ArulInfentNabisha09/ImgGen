b777e3a67f311bf38288e6191df2417dee53fdf0
devakirubai14 <dkjohn141@gmail.com>
2026-07-27 19:04:43 +0530

    Initial commit: Added Vision LLM carousel redesign engine with advanced HTML blocks and Canva PDF export

--- server/test_cobalt.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-axios.post('https://api.cobalt.tools/api/json', { url: 'https://www.instagram.com/p/Dax2J2LEl5G/' }, {
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'User-Agent': 'Mozilla/5.0'
-  }
-}).then(r => console.log(JSON.stringify(r.data, null, 2)))
-  .catch(e => console.log(e.response ? e.response.data : e.message));
--- server/test_igdl.js
+++
@@ -1,5 +0,0 @@
-const instagramDl = require('instagram-url-direct');
-
-instagramDl('https://www.instagram.com/p/Dax2J2LEl5G/')
-  .then(res => console.log(JSON.stringify(res, null, 2)))
-  .catch(err => console.error(err));
